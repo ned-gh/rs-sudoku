58c4edd45f8f456cbfddab2ca3483c592bd75030
ned <ned.git@gmail.com>
2024-07-08 23:37:28 +1000

    Minimal vite + React working with imported wasm lib.

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,40 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import * as wasm from "wasm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const bd = "300967001040302080020000070070000090000873000500010003004705100905000207800621004";
   const grid = wasm.get_grid_from_bd_str(bd);
   console.log(grid);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
   )
 }
 
